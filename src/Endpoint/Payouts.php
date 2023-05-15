015c788f461fa619ffab262de4f07ed60587ac1d
Ayatulloh Ahad R <kawoel@gmail.com>
2023-05-16 01:29:06 +0700

    payout adding header token to request payout

--- src/Endpoint/Payouts.php
+++ src/Endpoint/Payouts.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Ay4t\NOWPayments\Endpoint;
+use Ay4t\NOWPayments\Authentication;
 use Ay4t\NOWPayments\Endpoint\AbstractEndpoint;
 
 /**
@@ -24,6 +25,29 @@ class Payouts extends AbstractEndpoint
      */
     public function create( array $data = [] ): array
     {
+        // jika $data['email'] not exists throw exception
+        if(!isset($data['email'])){
+            throw new \Exception('Email not found');
+        }
+
+        // jika $data['password'] not exists throw exception
+        if(!isset($data['password'])){
+            throw new \Exception('Password not found');
+        }
+
+        $clientAuth = $this->client->auth->create([
+            'email'     => $data['email'],
+            'password'  => $data['password']
+        ]);
+        
+        if(!empty($clientAuth['token'])){
+            $headerToken = $clientAuth['token'];
+        } else {
+            throw new \Exception('Token not found');
+        }
+
+        $this->client->setHeaderToken($headerToken);
+
         return $this->request(parent::METHOD_POST, null, $data);
     }
 }
